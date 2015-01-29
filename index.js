1a54a31b433110054b2647fbf832de5e15beb805
Dan Abramov <dan.abramov@gmail.com>
2015-01-30 01:37:02 +0300

    Add initial proof of concept

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path'),
+    utils = require('loader-utils'),
     SourceNode = require('source-map').SourceNode,
     SourceMapConsumer = require('source-map').SourceMapConsumer,
     makeIdentitySourceMap = require('./makeIdentitySourceMap');
@@ -10,7 +11,9 @@ module.exports = function (source, map) {
     this.cacheable();
   }
 
-  var separator = '\n\n',
+  var options = utils.parseQuery(this.query),
+      filename = path.basename(this.resourcePath),
+      separator = '\n\n',
       appendText,
       node,
       result;
@@ -19,6 +22,69 @@ module.exports = function (source, map) {
     '/* FLUX HOT LOADER */',
     'if (module.hot) {',
       '(function () {',
+        'if (!module.exports) {',
+        '  return;',
+        '}',
+
+        'var canRewind = !!module.exports.dispatchToken;',
+        'if (!canRewind) {',
+          'console.warn(' + JSON.stringify(filename) + ' + " is missing a dispatchToken on module.exports.");',
+        '}',
+
+        'if (typeof module.exports.emitChange !== "function") {',
+          'console.warn(' + JSON.stringify(filename) + ' + " does not appear to be an event emitter.");',
+          'return;',
+        '}',
+
+        'var dispatcher = require(' + JSON.stringify(options.dispatcher) + '),',
+            'actions = [],',
+            'spyListenerId;',
+
+        'module.hot.dispose(function (data) {',
+          'if (canRewind) {',
+            'dispatcher.unregister(spyListenerId);',
+            'dispatcher.unregister(module.exports.dispatchToken);',
+          '}',
+
+          'data.dispatchToken = module.exports.dispatchToken;',
+          'data.events = module.exports._events;',
+          'data.actions = actions;',
+        '});',
+
+        'if (canRewind) {',
+          'spyListenerId = dispatcher.register(function (payload) {',
+            'actions.push(payload);',
+          '});',
+        '}',
+
+        'if (module.hot.data) {',
+          'module.exports._events = module.hot.data.events;',
+        '}',
+
+        'if (module.hot.data && canRewind) {',
+          'actions = module.hot.data.actions;',
+
+          'var callbacks = dispatcher["$Dispatcher_callbacks"],',
+              'cb = callbacks[module.exports.dispatchToken];',
+
+          'callbacks[module.hot.data.dispatchToken] = cb;',
+          'delete callbacks[module.exports.dispatchToken];',
+          'module.exports.dispatchToken = module.hot.data.dispatchToken;',
+
+          'var emitChange = module.exports.emitChange,',
+              'waitFor = dispatcher.waitFor;',
+
+          'module.exports.emitChange = function () {}',
+          'dispatcher.waitFor = function () {}',
+
+          'try {',
+            'actions.forEach(cb);',
+          '} finally {',
+            'module.exports.emitChange = emitChange;',
+            'dispatcher.waitFor = waitFor;',
+            'setTimeout(emitChange);',
+          '}',
+        '}',
       '})();',
     '}'
   ].join('\n');
